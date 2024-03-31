aa5ee35479ac8aeba0a31c375a68f3ba09fa668b
Michal Cz <michal.czechowicz7@gmail.com>
2024-03-31 22:06:57 +0200

    Added api docs for users

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
-from rest_framework import status
+from drf_spectacular.utils import extend_schema, inline_serializer
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
@@ -8,9 +9,34 @@ from users.serializers import UserLoginSerializer, UserRegisterSerializer
 
 
 class UserRegisterView(APIView):
+    """Register new user with username and password."""
+
     permission_classes = []
     authentication_classes = []
 
+    @extend_schema(
+        request=UserRegisterSerializer,
+        responses={
+            200: inline_serializer(
+                name="RegisterValidResponse",
+                fields={
+                    "message": serializers.CharField(
+                        default="User created successfully."
+                    ),
+                },
+            ),
+            400: inline_serializer(
+                name="RegisterInvalidResponse",
+                fields={
+                    "username": serializers.ListField(
+                        child=serializers.CharField(
+                            default="user with this username already exists."
+                        )
+                    )
+                },
+            ),
+        },
+    )
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -20,9 +46,39 @@ class UserRegisterView(APIView):
 
 
 class UserLoginView(APIView):
+    """Login user with username and password."""
+
     permission_classes = []
     authentication_classes = []
 
+    @extend_schema(
+        request=UserLoginSerializer,
+        responses={
+            200: inline_serializer(
+                name="LoginValidResponse",
+                fields={
+                    "message": serializers.CharField(default="Login successful."),
+                    "user": inline_serializer(
+                        name="User",
+                        fields={
+                            "id": serializers.IntegerField(default=1),
+                            "username": serializers.CharField(default="test_user"),
+                        },
+                    ),
+                    "jwt_access_token": serializers.CharField(default="jwt_access"),
+                    "jwt_refresh_token": serializers.CharField(default="jwt_refresh"),
+                },
+            ),
+            400: inline_serializer(
+                name="LoginInvalidResponse",
+                fields={
+                    "message": serializers.CharField(
+                        default="Missing username or password."
+                    ),
+                },
+            ),
+        },
+    )
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
 
@@ -59,9 +115,22 @@ class UserLoginView(APIView):
 
 
 class UserLogoutView(APIView):
+    """Logout currently authenticated user."""
+
     permission_classes = []
     authentication_classes = []
+    serializer_class = None
 
+    @extend_schema(
+        responses={
+            200: inline_serializer(
+                name="LogoutValidResponse",
+                fields={
+                    "message": serializers.CharField(default="User logged out."),
+                },
+            )
+        }
+    )
     def post(self, request):
         logout(request)
         response_data = {"message": "User logged out."}
@@ -69,6 +138,21 @@ class UserLogoutView(APIView):
 
 
 class UserDetailView(APIView):
+    """Get currently authenticated user's details."""
+
+    serializer_class = None
+
+    @extend_schema(
+        responses={
+            200: inline_serializer(
+                name="UserDetailValidResponse",
+                fields={
+                    "id": serializers.IntegerField(default=1),
+                    "username": serializers.CharField(default="test_user"),
+                },
+            ),
+        }
+    )
     def get(self, request):
         response_data = {"id": request.user.id, "username": request.user.username}
         return Response(response_data, status=status.HTTP_200_OK)
